fef85a2b5d2bdffd287ffd028717d738989a436e
leandro-hd <leandrohg2003@gmail.com>
2021-05-13 11:32:34 -0300

    Estruturando ambiente Node.js e PostgreSQL

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+require("dotenv/config");
+
+module.exports = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: "postgres",
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
